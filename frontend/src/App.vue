5924e61991ff644438d616e2d3dfb01204ea9a3c
Kalousu <kevinbecker848@gmail.com>
2026-02-19 15:11:02 +0100

    commit frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import square from './assets/square.png'
+</script>
+
+<template>
+	<h1>Tamagochi: CQRS + EDA</h1>
+	<div>
+		<img :src="square"/>
+	</div>
+	<div>
+		<p>Hunger: xxx</p>
+		<p>Sleep: xxx</p>
+	</div>	
+	<div style="display: flex; justify-content: center; gap: 30px;">
+		<button>a</button>
+		<button>b</button>
+	</div>
+</template>
+
+<style scoped>
+</style>
