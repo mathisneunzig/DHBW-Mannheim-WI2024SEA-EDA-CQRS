08dbb15ae4d723e5a416875d0ff92cbed96a4cf2
I750536 <jonas.thaddey@sap.com>
2026-02-19 21:28:28 +0100

    Frontend optimization

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,30 @@
 <script setup>
 import square from './assets/square.png'
+import { ref } from 'vue'
+import happy from './assets/tamagochi/happy.gif'
+import eating from './assets/tamagochi/eating.gif'
+import playing from './assets/tamagochi/eating.gif'
+import sad from './assets/tamagochi/eating.gif'
+import sleeping from './assets/tamagochi/eating.gif'
+import shappy from './assets/tamagochi/eating.gif'
+import ssad from './assets/tamagochi/eating.gif'
+import vhappy from './assets/tamagochi/eating.gif'
+import hungerbar from './assets/hungerbar.png'
+import boredbar from './assets/boredbar.png'
+import tiredbar from './assets/tiredbar.png'
+
+const currentGif = ref(happy)
 </script>
 
 <template>
-	<h1>Tamagochi: CQRS + EDA</h1>
-	<div>
-		<img :src="square"/>
+	<h1>SquirtleGotchi</h1>
+	<div style="position: relative; display: inline-block;">
+		<img :src="currentGif" class="tamagochi-gif"/>
 	</div>
 	<div>
-		<p>Hunger: xxx</p>
-		<p>Sleep: xxx</p>
-		<p>Boredom: xxx</p>
+		<p><img :src="hungerbar" class="status-icon"/> Hunger: xxx</p>
+		<p><img :src="tiredbar" class="status-icon"/> Sleep: xxx</p>
+		<p><img :src="boredbar" class="status-icon"/> Boredom: xxx</p>
 	</div>	
 	<div style="display: flex; justify-content: center; gap: 30px;">
 		<button>Feed</button>
@@ -20,4 +34,16 @@ import square from './assets/square.png'
 </template>
 
 <style scoped>
+.tamagochi-gif {
+	border: 10px solid #135d8b;
+	border-radius: 8px;
+	padding: 10px;
+	background-color: #f0f0f0;
+	display:block;
+}
+.status-icon {
+	height: 1em;
+	width: auto;
+	vertical-align: middle;
+}
 </style>
